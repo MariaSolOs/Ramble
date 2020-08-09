57f1a80daa240376738b89dd9db255bcb4485bb7
Maria Solano <majosolano99@gmail.com>
2020-08-09 13:56:57 -0400

    Fix tasks

--- client/src/pages/Creators/CreatorDashboard/Notifications/Notifications.js
+++ client/src/pages/Creators/CreatorDashboard/Notifications/Notifications.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 
 import Fab from '@material-ui/core/Fab';
+import CheckIcon from '@material-ui/icons/Check';
 
 import {makeStyles} from '@material-ui/core/styles';
 const useStyles = makeStyles(() => ({
@@ -20,7 +21,9 @@ const useStyles = makeStyles(() => ({
     },
     notifIcon: {
         fontWeight: 'bold',
-        marginRight: '1rem'
+        marginRight: '1rem',
+        width: 50, height: 50,
+        cursor: 'default'
     }
 }));
 
@@ -29,14 +32,24 @@ const timeRegex = /((1[0-2]|[1-9])(:30)?([AP][M]))/g;
 const Notifications = (props) => {
     const classes = useStyles();
 
-    const notifs = useSelector(state => state.user.notifications);
+    //const notifs = useSelector(state => state.user.notifications);
+    const notifs = [
+        {message: 'Your experience "Maria and Tequila" is happening ' +
+                  "tomorrow at 10:30PM. Don't forget!",
+        category: 'Creator-ExperienceReminder',
+        _id: 0},
+        {message: 'Your experience "Dancing with your cat" has been approved',
+        category: 'Creator-ExperienceDecision',
+        _id: 1,
+    }];
 
     const getNotifIcon = (notif) => {
         switch(notif.category) {
             case 'Creator-ExperienceReminder':
                 const time = notif.message.match(timeRegex)[0];
-                console.log(time);
-                return time;
+                return time.slice(0, -2);
+            case 'Creator-ExperienceDecision': 
+                return <CheckIcon/>
             default: ;
         }
     }
@@ -47,7 +60,8 @@ const Notifications = (props) => {
                 <li 
                 key={notif._id}
                 className={classes.notif}>
-                    <Fab classes={{ root: classes.notifIcon }}>
+                    <Fab classes={{ root: classes.notifIcon }}
+                    disableRipple>
                         {getNotifIcon(notif)}
                     </Fab>
                     {notif.message}

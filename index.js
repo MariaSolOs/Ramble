7ba2a601d06bec0c0aa6d8110b911fd94e487426
Maria Solano <majosolano99@gmail.com>
2021-05-21 14:41:10 -0400

    Add static

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,

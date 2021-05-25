7ab099e1025cde9a4f74a8fb188f7bbfb35d1555
Maria Solano <majosolano99@gmail.com>
2021-05-24 23:21:45 -0400

    Heroku fix

--- client/src/components/Navbar/ProfileMenu.styles.ts
+++ client/src/components/Navbar/ProfileMenu.styles.ts
@@ -1,6 +1,6 @@
 import { createStyles, Theme } from '@material-ui/core/styles';
 
-const styles = (theme: Theme) => createStyles({
+const styles = (_: Theme) => createStyles({
 });
 
 export default styles;

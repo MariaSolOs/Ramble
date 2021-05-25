b51570897bebb0f01177acf7c657346579c758a6
Maria Solano <majosolano99@gmail.com>
2021-05-25 10:47:31 -0400

    Heroku fix

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const typeDefs = require('./graphql/schema');
 const resolvers = require('./graphql/resolvers');
 const path = require('path');
 const express = require('express');
-
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));

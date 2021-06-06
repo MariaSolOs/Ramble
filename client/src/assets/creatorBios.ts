fef312ab294775c895fd8bb9969143f9f223650d
Maria Solano <majosolano99@gmail.com>
2021-06-06 18:35:17 -0400

    Fix map

---
+++ client/src/assets/creatorBios.ts
@@ -0,0 +1,15 @@
+// type CreatorData = Record<'name' | 'city' | 'bio' | 'imageUrl', string>;
+
+// const biosData: CreatorData[] = [{
+//         name: 'Kevin', 
+//         city: 'Montréal', 
+//         bio: {
+//             'I\'m a bartender and an ice carver in one of Montreal\'s coolest cocktail bars. I consider myself a creative and devoted mind and I have yet to quench my thirst for everything mixology.'
+//         }, 
+//         imageUrl: `${process.env.REACT_APP_CLOUDINARY_BASE_URI}c_thumb,g_face,h_500,w_500/v1614733940/Ramble/Creators/kevin_ezussv.jpg`
+//     }
+// ];
+
+// export default biosData;
+
+export {}
--- client/src/pages/Creator/BecomeACreator/BecomeACreator.tsx
+++ client/src/pages/Creator/BecomeACreator/BecomeACreator.tsx
@@ -1,5 +1,6 @@
 import { useLanguageContext } from '../../../context/languageContext';
 
+// import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Button from '../../../components/GradientButton/GradientButton';
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -11,20 +12,23 @@ const BecomeACreator = () => {
     const classes = useStyles();
 
     return (
-        <div className={classes.landingSlide}>
-            <div>
-                <h1 className={classes.title}>{text.becomeTitle}</h1>
-                <h1 className={classes.title}>{text.shareTitle}</h1>
-                <h1 className={classes.title}>{text.getPaidTitle}</h1>
-                <Button className={classes.getStartedButton} variant="creator">
-                    {text.getStarted}
-                </Button>
+        <>
+            <div className={classes.landingSlide}>
+                <div>
+                    <h1 className={classes.title}>{text.becomeTitle}</h1>
+                    <h1 className={classes.title}>{text.shareTitle}</h1>
+                    <h1 className={classes.title}>{text.getPaidTitle}</h1>
+                    <Button className={classes.getStartedButton} variant="creator">
+                        {text.getStarted}
+                    </Button>
+                </div>
+                <img 
+                className={classes.landingImg}
+                alt="Cooking with Sidney"
+                src={`${process.env.REACT_APP_CLOUDINARY_BASE_URI}c_fill,h_600,w_500/v1622837493/Ramble/Homepage/landing.jpg`} />
             </div>
-            <img 
-            className={classes.landingImg}
-            alt="Cooking with Sidney"
-            src={`${process.env.REACT_APP_CLOUDINARY_BASE_URI}c_fill,h_600,w_500/v1622837493/Ramble/Homepage/landing.jpg`} />
-        </div>
+
+        </>
     );
 }
 

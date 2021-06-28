5bd5ef126a9ad6f22cdbfe091569bd560ad8fdcb
Maria Solano <majosolano99@gmail.com>
2021-06-27 22:33:54 -0400

    FIx scrolling

--- client/src/pages/Experience/BuilderSlides/Review/Review.styles.ts
+++ client/src/pages/Experience/BuilderSlides/Review/Review.styles.ts
@@ -4,12 +4,8 @@ import type { Theme } from '@material-ui/core/styles';
 const styles = (theme: Theme) => createStyles({
     experienceContainer: {
         width: 450,
-        height: 460,
 
-        [theme.breakpoints.down('xs')]: {
-            width: '100%',
-            height: 'calc(100% + 60px)'
-        }
+        [theme.breakpoints.down('xs')]: { width: '100%' }
     }
 });
 

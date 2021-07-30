82190c191a938306319f835f9c1bc43130ef48c7
Maria Solano <majosolano99@gmail.com>
2021-07-29 21:28:31 -0400

    Update stripe keys

--- server/index.ts
+++ server/index.ts
@@ -11,6 +11,8 @@ import path from 'path';
 import emailRoutes from './emailsAPI';
 import stripeRoutes from './stripeAPI';
 
+// Make sure bin scripts run in Heroku
+
 const app = express();
 
 app.use(cors({

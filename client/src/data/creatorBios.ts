43e1f838c703bb9f2289d8ffcdb375f4de5ec51a
Maria Solano <majosolano99@gmail.com>
2021-08-07 17:53:42 -0400

    Delete Julie A

--- client/src/data/creatorBios.ts
+++ client/src/data/creatorBios.ts
@@ -14,14 +14,6 @@ const biosData: CreatorData[] = [
             en: "I'm a bartender and an ice carver in one of Montreal's coolest cocktail bars. I consider myself a creative and devoted mind and I have yet to quench my thirst for everything mixology.",
             fr: "Je travaille dans l'un des bars à cocktails les plus \"cool\" de Montréal. Après toutes ces années, je ne suis toujours pas arrivé à étancher ma soif de tout ce qui touche à la mixologie."
         }
-    }, 
-    {
-        name: 'Julie Anna',
-        imageUrl: `${process.env.REACT_APP_CLOUDINARY_BASE_URI}c_thumb,g_face,h_500,w_500/v1620764151/Ramble/Experiences/egc0aafohpii1gprogca.jpg`,
-        bio: {
-            en: "I'm a young mixologist-restaurateur, passionate and hyperactive. I'm huge fan of complex simplicity, human connections and discovery. Enter my funky world and let's get to know each other during happy hour!",
-            fr: "Jeune restauratrice cocktologue, passionnée et hyperactive. Grande fan de simplicité complexe, de connexions humaines et de découvertes. Entrez dans mon univers funky, apprenons à se connaître en prenant l'apéro!"
-        }
     },
     {
         name: 'Pierre & Sidney',
